b0f6685ebcd769aca5ff246ed8ba606d977d35c4
Sam Boyer <samboyer@users.noreply.github.com>
2021-09-20 15:39:06 +0100

    Migrate index page to PHP

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<!DOCTYPE html><!--  This site was created in Webflow. http://www.webflow.com  -->
-<!--  Last Published: Thu Sep 09 2021 18:41:45 GMT+0000 (Coordinated Universal Time)  -->
-<html data-wf-page="61312cc3d820c1efda819fe6" data-wf-site="61312cc3d820c1211b819fe5">
+<?php
+  require __DIR__ . '/wp-blog-header.php';
+?>
+<!DOCTYPE html>
+<html>
 <head>
   <meta charset="utf-8">
   <title>Oxford Recording Society</title>
@@ -9,8 +11,8 @@
   <link href="css/normalize.css" rel="stylesheet" type="text/css">
   <link href="css/webflow.css" rel="stylesheet" type="text/css">
   <link href="css/ors-webflow-help.webflow.css" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Roboto Mono:300,regular,500,600,700,100italic,200italic,300italic,italic,500italic,600italic,700italic"]  }});</script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script> -->
+  <!-- <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Roboto Mono:300,regular,500,600,700,100italic,200italic,300italic,italic,500italic,600italic,700italic"]  }});</script> -->
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
   <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
@@ -27,9 +29,14 @@
     -webkit-text-stroke: 0.8px #FFFFFF;
   }
 </style>
+<?php wp_head(); ?>
+
 </head>
 <body>
-  <div data-collapse="medium" data-animation="default" data-duration="400" id="nav-bar" role="banner" class="nav-bar nav-bar-landing w-nav"><img src="images/ORSlogowhite.png" loading="lazy" id="nav-logo" alt="" class="nav-logo nav-logo-landing">
+  <?php
+  get_template_part( 'template-parts/header/site-header' );
+  ?>
+  <!-- <div data-collapse="medium" data-animation="default" data-duration="400" id="nav-bar" role="banner" class="nav-bar nav-bar-landing w-nav"><img src="images/ORSlogowhite.png" loading="lazy" id="nav-logo" alt="" class="nav-logo nav-logo-landing">
     <nav role="navigation" id="nav-menu" class="nav-menu nav-menu-landing w-nav-menu">
       <a href="index.html" aria-current="page" class="nav-link w-nav-link w--current">Home</a>
       <a href="ourteam.html" class="nav-link w-nav-link">OUR TEAM</a>
@@ -43,7 +50,7 @@
     <div class="w-nav-button">
       <div class="icon w-icon-nav-menu"></div>
     </div>
-  </div>
+  </div> -->
   <div id="landing" class="landing">
     <div class="landing-content2">
       <div class="div-block-13">
