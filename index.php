715148f2ecabd27df94e541e9eaff34cd52ab616
Sam Boyer <samboyer@users.noreply.github.com>
2021-10-05 22:38:03 +0100

    Remove static footer & extra marquee

--- index.php
+++ index.php
@@ -79,9 +79,6 @@
       </div>
     </div>
   </div>
-  <div class="marquee">
-    <p class="paragraph marquee">ORS // UNIVERSITY OF OXFORD&#x27;S STUDENT MUSIC INDUSTRY HUB    ORS // UNIVERSITY OF OXFORD&#x27;S STUDENT MUSIC INDUSTRY HUB    ORS // UNIVERSITY OF OXFORD&#x27;S STUDENT MUSIC INDUSTRY HUB    ORS // UNIVERSITY OF OXFORD&#x27;S STUDENT MUSIC INDUSTRY HUB    ORS // UNIVERSITY OF OXFORD&#x27;S STUDENT MUSIC INDUSTRY HUB</p>
-  </div>
   <div data-delay="4000" data-animation="slide" data-autoplay="1" data-duration="500" data-infinite="1" class="slider-3 w-slider">
     <div class="div-block-6"></div>
     <div class="mask w-slider-mask">
@@ -360,46 +357,7 @@
       </div>
     </div>
   </div>
-  <div class="footer">
-    <div class="footer-container">
-      <div class="footer-links-section">
-        <div class="footer-logo"><img src="images/ORSlogowhite.png" loading="lazy" alt="" class="image-41"></div>
-        <div class="footer-links">
-          <h1 class="heading-21">// useful links</h1>
-          <ul role="list" class="w-list-unstyled">
-            <li class="footer-list">Contact Us</li>
-            <li class="footer-list">Constitution</li>
-            <li class="footer-list">Rules &amp; Policies</li>
-            <li class="footer-list">Useful Documents</li>
-          </ul>
-        </div>
-        <div class="footer-links">
-          <h1 class="heading-21">// FOR STUDENTS</h1>
-          <ul role="list" class="w-list-unstyled">
-            <li class="footer-list">Get Involved</li>
-            <li class="footer-list">Michaelmas Termcard</li>
-            <li class="footer-list">Become a member</li>
-            <li class="footer-list">Member Hub - Login</li>
-          </ul>
-        </div>
-        <div class="footer-links">
-          <h1 class="heading-21">// EXTERNAL INFORMATION</h1>
-          <ul role="list" class="w-list-unstyled">
-            <li class="footer-list">For Venues &amp; Studios</li>
-            <li class="footer-list">For Patrons</li>
-            <li class="footer-list">For Sponsors</li>
-            <li class="footer-list">For Speakers</li>
-            <li class="footer-list">Society &amp; Ball Committees</li>
-          </ul>
-        </div>
-      </div>
-      <div class="div-block-60">
-        <div class="div-block-61"></div>
-        <p class="paragraph-12"><span class="text-span-8">Welcome to the ORS!</span> This site is still under development, so things might not be 100% stable right now. If you find anything wrong, or have other feedback, please <a href="https://forms.office.com/r/xyhn6mxSr7" target="_blank" class="bug-link">let us know here</a>.</p>
-        <p class="paragraph-13">© Oxford Recording Society, 2021, site by ORS Dev Team</p>
-      </div>
-    </div>
-  </div>
+
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=61312cc3d820c1211b819fe5" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
