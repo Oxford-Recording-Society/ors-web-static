f60f3db85f5c7e231a09ba63eac6f2070c086d32
Sam Boyer <samboyer@users.noreply.github.com>
2021-10-28 21:47:50 +0100

    Add /membership page

---
+++ membership.php
@@ -0,0 +1,94 @@
+<?php
+  require __DIR__ . '/wp-blog-header.php';
+?>
+<!DOCTYPE html>
+<html data-wf-page="61391bc1bc7bea1aca6d9505" data-wf-site="61312cc3d820c1211b819fe5">
+<head>
+  <meta charset="utf-8">
+  <title>Membership Benefits | Oxford Recording Society</title>
+  <meta content="Team-test" property="og:title">
+  <meta content="Team-test" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="/css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="/css/ors-webflow-help.webflow.css" rel="stylesheet" type="text/css">
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Roboto Mono:300,regular,500,600,700,100italic,200italic,300italic,italic,500italic,600italic,700italic"]  }});</script> -->
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="/images/webclip.png" rel="apple-touch-icon">
+  <style>
+.outlinetext {
+    color: rgba(0, 0, 0, 0);
+    -webkit-text-stroke: 0.4px #FFFFFF;
+  }
+</style>
+<?php wp_head(); ?>
+</head>
+<body>
+
+<?php get_template_part( 'template-parts/header/site-header' ); ?>
+
+<div class="ors-page-membership-container">
+  <div class="ors-page-membership-box-1">
+    <h1 class="ors-page-membership-title uppercase">ORS Membership is for budding:</h1>
+    <i><span class="uppercase">Performers, producers, songwriters, artists, sound engineers, designers, videographers/photographers, music execs, managers, events planners</span> &amp; anyone else interested in the music industry.</i>
+    <p>We are a new society - so with a lifetime membership, as we grow over your time at Oxford, you'll get even more benefits. This term we're offering:
+    <u>social events</u>,
+    an Oxford <u>music industry mini social network</u>,
+    <u>studio recording</u> opportunities
+    and our own <u>record label</u>.</p>
+
+    <a class="ors-button" href=""><img src="/images/download-solid.svg" /> Membership Brochure</a>
+  </div>
+
+  <div class="team-member-3 team-desc">
+    <div class="ors-page-membership-box coolBox">
+      <h1 class="team-title">Lifetime Membership</h1>
+      <h2>Get directly involved in Oxford's student-led music industry</h2>
+      <h1 class="price">£15</h1>
+      <h2 class="price"><s>£25</s> <span class="price-alert">Michaelmas Discount</span></h2>
+
+      <h3>Current Benefits</h3>
+      <ul>
+        <li>Free access to Member socials & Member database - connect with ORS members, musicians, and find music industry collab opportunities</li>
+        <li>Access to our Opportunities feed - a stream of musical opportunities (songwriting/production collabs, performance opportunities (open mic, busking etc.), making art & videos, management & events planning, and more)</li>
+        <li>Free access to production & performing workshops, and speaker events (next term and beyond)</li>
+        <li>Record, produce, mix and master your work with a producer/engineer at SAE studios</li>
+        <li>Release your music through OX Records (album art, mixing/mastering, distribution & promotion services), or get involved behind the scenes</li>
+      </ul>
+      <h3>Future Benefits</h3>
+      <ul>
+        <li>Rent music equipment (sound systems, midi controllers, mics etc.) for free</li>
+        <li>Access more recording sessions and practice rooms at other studios/facilities</li>
+        <li>Reduced prices for ORS club &amp; gig tickets</li>
+        <li>Meet, &amp; hear from music industry professionals (artists, producers/songwriters, label execs, A&R etc.)</li>
+      </ul>
+
+      <a class="ors-button" href="/register">Become a Member</a>
+    </div>
+  </div>
+  <div class="team-member-3 team-desc">
+    <div class="ors-page-membership-box">
+      <h1 class="team-title">Join the Mailing List</h1>
+      <h2>Get updates on Oxford's student-led music industry</h2>
+      <h1 class="price">FREE</h1>
+      <ul>
+        <li>Get notified of all our events and opportunities via email</li>
+        <li>Join our Facebook groups and meet other Oxford students interested in the music industry</li>
+        <li>Buy tickets to attend all our workshops, socials and speaker events</li>
+      </ul>
+      <a class="ors-button" href="https://forms.office.com/Pages/ResponsePage.aspx?id=G96VzPWXk0-0uv5ouFLPkStGIYr39BFEswJRxaDWKN1URU1HTUNCRU5HUlpYMDFXWTRKNzVaVzFGOC4u" target="_blank">Sign Up</a>
+    </div>
+  </div>
+
+</div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=61312cc3d820c1211b819fe5" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+
+<?php get_footer(); ?>
+
+</body>
+</html>
